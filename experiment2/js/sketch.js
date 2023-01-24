6d99dcea28b49b423fa7849a95de1641f9d3067c
nlaureta <73408370+nlaureta@users.noreply.github.com>
2023-01-23 19:26:17 -0800

    push
    push

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,67 +1,154 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Nathan Laureta
+// Date: 1/24/2023
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+// P_2_1_2_01
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// Globals
-let myInstance;
-let canvasContainer;
+/**
+ * changing size and position of circles in a grid
+ *
+ * MOUSE
+ * position x          : circle position
+ * position y          : circle size
+ * left click          : random position
+ *
+ * KEYS
+ * s                        : save png
+ * hold shift/left click    : draw
+ * space(toggle)            : pause the cirlces 
+ * backspace                : clear drawing
+ * 1                        : color cirlces red
+ * 2                        : color circles blue
+ * 3                        : color cirlces green
+ * 4(toggle)                : color circles and background randomly every click
+ * 0                        : switch to default colors
+ * connect the dots with the paused random circles
+ */
+'use strict';
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+var tileCount = 20;
+var actRandomSeed = 0;
 
-    myMethod() {
-        // code to run when method is called
-    }
-}
+var circleAlpha = 130;
+var circleColor;
+var points = []; 
+let pause = false;
+let rgb = false;
+let bgColor = 0;
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(1280, 900);
+  noFill();
+  strokeWeight(5);
+  circleColor = color(0, 0, 0, circleAlpha);
+  bgColor = 255;
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  translate(width / tileCount / 2, height / tileCount / 2);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  background(bgColor);
+
+  stroke(circleColor);
+    
+  strokeWeight(mouseY / 60);
+ 
+  beginShape();
+  for(var i in points) {
+    // grab the point by index
+    var one_point = points[i];
+    curveVertex(one_point.x, one_point.y);
+  }
+	endShape();
+
+    if(rgb) {
+        bgColor = color(random(255), random(255), random(255));
+        circleColor = color(random(255), random(255),random(255), circleAlpha);
+    }
+
+  randomSeed(actRandomSeed);
+
+    for (var gridY = 0; gridY < tileCount; gridY++) {
+        for (var gridX = 0; gridX < tileCount; gridX++) {
+
+            var posX = width / tileCount * gridX;
+            var posY = height / tileCount * gridY;
+            var shiftX = random(-mouseX, mouseX) / 20;
+            var shiftY = random(-mouseX, mouseX) / 20;
+
+            ellipse(posX + shiftX, posY + shiftY , mouseY / 15, mouseY / 15);
+
+        }
+    }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+    if(!keyIsDown(SHIFT) && !pause)
+        actRandomSeed = random(100000);
 }
+
+function mouseDragged(){
+    // create an object as empty point
+    if(keyIsDown(SHIFT)) {
+        beginShape();
+  for(var i in points) {
+    // grab the point by index
+    var one_point = points[i];
+    curveVertex(one_point.x, one_point.y);
+  }
+	endShape();
+    var one_point = {};
+    one_point.x = mouseX - 35;
+    one_point.y = mouseY ;
+    
+    // add the point to the array
+    points.push(one_point);
+    }
+}
+
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  if(key == ' ') {
+    //print('Space pressed');
+    pause = !pause;
+  }
+  if (key == '1') circleColor = color(255, 0, 0, circleAlpha);
+  if (key == '2') circleColor = color(0, 255, 0, circleAlpha);
+  if (key == '3') circleColor = color(0, 0, 255, circleAlpha);
+  if (key == '4') rgb = !rgb;
+  if (key == '0') {
+        circleColor = color(0, 0, 0, circleAlpha);
+        bgColor = 255;
+    }
+  if (keyCode === BACKSPACE) {
+    //print("backspace");
+    points = [];
+  }
+  if(pause) {
+    noLoop();
+  }else 
+    loop(); 
+  
+ 
+}
+
