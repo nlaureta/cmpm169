70bd0a6f6823f0f93885a54b9fb1818bcb97e8f8
nlaureta <nate.laureta@gmail.com>
2023-06-28 19:38:53 -0700

    homepage for cmpm169

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,154 +1,107 @@
 // sketch.js - purpose and description here
 // Author: Nathan Laureta
-// Date: 1/24/2023
+// Date: 1/31/2022
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
-// P_2_1_2_01
-//
-// Generative Gestaltung – Creative Coding im Web
-// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
-// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
-// with contributions by Joey Lee and Niels Poldervaart
-// Copyright 2018
-//
-// http://www.generative-gestaltung.de
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Constants - User-servicable parts
+// In a longer project I like to put these in a separate file
 
+//credit: line pattern and middle circle by https://editor.p5js.org/Sylvesterd2/sketches/B1kLLJ82b
 /**
- * changing size and position of circles in a grid
+ * lines and circle
  *
  * MOUSE
- * position x          : circle position
- * position y          : circle size
- * left click          : random position
+ * left click hold          : change color to color picker
  *
  * KEYS
  * s                        : save png
- * hold shift/left click    : draw
- * space(toggle)            : pause the cirlces 
- * backspace                : clear drawing
- * 1                        : color cirlces red
- * 2                        : color circles blue
- * 3                        : color cirlces green
- * 4(toggle)                : color circles and background randomly every click
- * 0                        : switch to default colors
- * connect the dots with the paused random circles
+ * 1(toggle)                : randomize color
+ * 2                        : color circle and line blue
+ * 3                        : color cirlce and line green
+ * 4                        : color circle and line red
+ * 5                        : color circle and line yellow
+ * 6                        : color circle and line from color picker
+ * 0                        : color circle and line black
  */
-'use strict';
 
-var tileCount = 20;
-var actRandomSeed = 0;
 
-var circleAlpha = 130;
-var circleColor;
-var points = []; 
-let pause = false;
-let rgb = false;
-let bgColor = 0;
+var x = 0;
+var y = 0;
+var gap = 30;
+var deg = 0;
+var circlePaint;
+var color_picker;
+var rgb = false;
 
 function setup() {
-  createCanvas(1280, 900);
-  noFill();
-  strokeWeight(5);
-  circleColor = color(0, 0, 0, circleAlpha);
-  bgColor = 255;
+	createCanvas(800,800);
+	background(35);
+    paint = 'blue';
+    circlePaint = 'blue';
+    color_picker = createColorPicker('#ed225d');
+	frameRate(60);
+	
+}
+
+function mousePressed() { //If you click the mouse, the lines and shapes will turn cyan.
+	paint = color_picker.color(); 
+}
+
+function mouseReleased() { //If you release the mouse click, the lines and shapes will turn red.
+	paint = circlePaint; 
 }
 
 function draw() {
-  translate(width / tileCount / 2, height / tileCount / 2);
+    //background(color_picker.color());
 
-  background(bgColor);
-
-  stroke(circleColor);
-    
-  strokeWeight(mouseY / 60);
- 
-  beginShape();
-  for(var i in points) {
-    // grab the point by index
-    var one_point = points[i];
-    curveVertex(one_point.x, one_point.y);
-  }
-	endShape();
-
+	spin();
     if(rgb) {
-        bgColor = color(random(255), random(255), random(255));
-        circleColor = color(random(255), random(255),random(255), circleAlpha);
+        paint = color(random(255), random(255),random(255));
     }
-
-  randomSeed(actRandomSeed);
-
-    for (var gridY = 0; gridY < tileCount; gridY++) {
-        for (var gridX = 0; gridX < tileCount; gridX++) {
-
-            var posX = width / tileCount * gridX;
-            var posY = height / tileCount * gridY;
-            var shiftX = random(-mouseX, mouseX) / 20;
-            var shiftY = random(-mouseX, mouseX) / 20;
-
-            ellipse(posX + shiftX, posY + shiftY , mouseY / 15, mouseY / 15);
-
-        }
-    }
+	maze();
 }
 
-function mousePressed() {
-    if(!keyIsDown(SHIFT) && !pause)
-        actRandomSeed = random(100000);
-}
-
-function mouseDragged(){
-    // create an object as empty point
-    if(keyIsDown(SHIFT)) {
-        beginShape();
-  for(var i in points) {
-    // grab the point by index
-    var one_point = points[i];
-    curveVertex(one_point.x, one_point.y);
-  }
-	endShape();
-    var one_point = {};
-    one_point.x = mouseX - 35;
-    one_point.y = mouseY ;
-    
-    // add the point to the array
-    points.push(one_point);
+function maze() { // This creates a random pattern of lines.
+    stroke(paint);
+    strokeWeight(2);
+    if (random(2) < 0.5) { // The if statement changes the direction of the lines.
+        line(x, y, x + gap, y + gap);
+    } else {
+        line(x, y + gap, x + gap, y);
     }
-}
 
-function keyReleased() {
-  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-  if(key == ' ') {
-    //print('Space pressed');
-    pause = !pause;
-  }
-  if (key == '1') circleColor = color(255, 0, 0, circleAlpha);
-  if (key == '2') circleColor = color(0, 255, 0, circleAlpha);
-  if (key == '3') circleColor = color(0, 0, 255, circleAlpha);
-  if (key == '4') rgb = !rgb;
-  if (key == '0') {
-        circleColor = color(0, 0, 0, circleAlpha);
-        bgColor = 255;
+    x = x + random(20) //This allows the lines to go across the canvas.
+    if (x > width) {
+        x = 0;
+        //y = y + gap;
     }
-  if (keyCode === BACKSPACE) {
-    //print("backspace");
-    points = [];
-  }
-  if(pause) {
-    noLoop();
-  }else 
-    loop(); 
-  
- 
+
+}
+			
+function spin() { //This creates the circle in the middle of the canvas.
+    scale(.5);
+    translate(width, height);
+    rotate(radians(deg));
+    deg++;
+    fill(paint);
+    rect(0, 0, 100, 100);
 }
 
+function keyPressed() { 
+    if (keyCode == 32) {
+        saveCanvas('', 'png');
+    }
+
+    if (key == '1') {
+        print("1 pressed");
+        rgb = !rgb;
+    }
+    if (key == '2') {paint = 'blue';circlePaint = 'blue';}
+    if (key == '3'){ paint = 'green';circlePaint = 'green';}
+    if (key == '4'){ paint = 'red';circlePaint = 'red';}
+    if (key == '5'){ paint = 'yellow';circlePaint = 'yellow';}
+    if (key == '6'){ paint = color_picker.color();circlePaint = color_picker.color();}
+    if (key == '0'){ paint = 'black';circlePaint = 'black';}
+} 
--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,6 +1,6 @@
 // sketch.js - purpose and description here
-// Author: Nathan Laureta
-// Date: 1/31/2022
+// Author: Your Name
+// Date:
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
@@ -8,100 +8,121 @@
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
 
-//credit: line pattern and middle circle by https://editor.p5js.org/Sylvesterd2/sketches/B1kLLJ82b
-/**
- * lines and circle
- *
- * MOUSE
- * left click hold          : change color to color picker
- *
- * KEYS
- * s                        : save png
- * 1(toggle)                : randomize color
- * 2                        : color circle and line blue
- * 3                        : color cirlce and line green
- * 4                        : color circle and line red
- * 5                        : color circle and line yellow
- * 6                        : color circle and line from color picker
- * 0                        : color circle and line black
- */
+//Credits: https://editor.p5js.org/Ruyi_Chen/sketches/H1UJM_0nQ
+//Song Used: Billy Joel - Piano Man https://www.youtube.com/watch?v=QwVjTlTdIDQ 
+
+let audio;
+let start = false;
+var img;
+var song;
+var fft;
+var button;
+var mic;
+let startButton;
+let vid;
+let canvas;
 
 
-var x = 0;
-var y = 0;
-var gap = 30;
-var deg = 0;
-var circlePaint;
-var color_picker;
-var rgb = false;
+function toggleSong() {
+  if (song.isPlaying()) {
+    song.pause();
+    vid.pause();
+  } else {
+    song.play();
+    vid.loop();
+  }
 
+}
+
+function preload() {
+  song = loadSound('pianoMan.mp3');
+  //img = loadImage('billy.jpg');
+  
+}
 function setup() {
-	createCanvas(800,800);
-	background(35);
-    paint = 'blue';
-    circlePaint = 'blue';
-    color_picker = createColorPicker('#ed225d');
-	frameRate(60);
-	
-}
+  canvas = createCanvas(1280, 720);
+  canvas.position(100,1050);
+  canvas.style('z-index', 1);
+  
+  angleMode(DEGREES);
+  //background(238,235,219);
+  //song.play();
+  //button = createButton('toggle');
+  //button.mousePressed(toggleSong);
+  fft = new p5.FFT(0.5, 64);
+  w = width / 64;
 
-function mousePressed() { //If you click the mouse, the lines and shapes will turn cyan.
-	paint = color_picker.color(); 
-}
+  vid = createVideo('cassette.mp4');
+  
+  vid.style('z-index', 0);
+  vid.position(100,1050);
+  
+  mic = new p5.AudioIn();
+  mic.start();
 
-function mouseReleased() { //If you release the mouse click, the lines and shapes will turn red.
-	paint = circlePaint; 
+  //fill(0);
+
+  
+  //vid.position(0,0);
+  
+  // startButton = createButton('click toggle to start);
+  // //startButton.position(100,100);
+  // startButton.mousePressed(playSong);
+
 }
 
 function draw() {
-    //background(color_picker.color());
+  if (start) {
+    return;
+  }
+  //image(img, 0, 0);
+  //background('rgba(0,255,0, 1)');
+  clear();
+  if(typeof fft != "undefined") {
+    //background(col.r,col.g,col.b);
+    var spectrum = fft.analyze();
+  
+  
+  stroke(random(255));
 
-	spin();
-    if(rgb) {
-        paint = color(random(255), random(255),random(255));
-    }
-	maze();
+  //noFill();
+  for (i = 0; i < spectrum.length; i++) {
+    var amp = spectrum[i];
+    var y = map(amp, 0, 356, height, 0);
+    strokeWeight(3);
+    line(i * w, y, w * i, height);
+  }
+
+  // var vol = mic.getLevel();
+  // fill(255, 0, 0);
+  // ellipse(width / 2, height / 2, 200, vol * 200);  
+  // fill(0);
+  // ellipse(width / 2, height / 2, 100, vol * 200);
+  }
 }
 
-function maze() { // This creates a random pattern of lines.
-    stroke(paint);
-    strokeWeight(2);
-    if (random(2) < 0.5) { // The if statement changes the direction of the lines.
-        line(x, y, x + gap, y + gap);
-    } else {
-        line(x, y + gap, x + gap, y);
-    }
-
-    x = x + random(20) //This allows the lines to go across the canvas.
-    if (x > width) {
-        x = 0;
-        //y = y + gap;
-    }
-
+function mousePressed() {
+  if(start)
+    toggleSong();
+  start = !start;
+  
 }
-			
-function spin() { //This creates the circle in the middle of the canvas.
-    scale(.5);
-    translate(width, height);
-    rotate(radians(deg));
-    deg++;
-    fill(paint);
-    rect(0, 0, 100, 100);
+function playSong() {
+  song.play();
 }
 
-function keyPressed() { 
-    if (keyCode == 32) {
-        saveCanvas('', 'png');
-    }
+var loader = document.querySelector(".loader");
+document.getElementById("audiofile").onchange = function(event) {
+    if(event.target.files[0]) {
+        if(typeof song != "undefined") { // Catch already playing songs
+            song.disconnect();
+            song.stop();
+            clear();
+        }
 
-    if (key == '1') {
-        print("1 pressed");
-        rgb = !rgb;
+        // Load our new song
+        song = loadSound(URL.createObjectURL(event.target.files[0]));
+        //song.play();
+        //loader.classList.add("loading");
     }
-    if (key == '2') {paint = 'blue';circlePaint = 'blue';}
-    if (key == '3'){ paint = 'green';circlePaint = 'green';}
-    if (key == '4'){ paint = 'red';circlePaint = 'red';}
-    if (key == '5'){ paint = 'yellow';circlePaint = 'yellow';}
-    if (key == '6'){ paint = color_picker.color();circlePaint = color_picker.color();}
-    if (key == '0'){ paint = 'black';circlePaint = 'black';}
-} 
+}
